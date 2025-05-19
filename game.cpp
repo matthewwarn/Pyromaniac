667f068925a74648165a942aae7884b7bb5eb293
Matthew Warn <78565473+matthewwarn@users.noreply.github.com>
2025-05-19 14:53:27 +1200

    Real Final Commit
    Forgot to switch the fullscreen on before creating the last release build :p

--- game.cpp
+++ game.cpp
@@ -84,7 +84,7 @@ bool Game::Initialise()
 	int bbHeight = 480;
 
 	m_pRenderer = new Renderer();
-	if (!m_pRenderer->Initialise(true, bbWidth, bbHeight))
+	if (!m_pRenderer->Initialise(false, bbWidth, bbHeight))
 	{
 		LogManager::GetInstance().Log("Renderer failed to initialise!");
 		return false;
